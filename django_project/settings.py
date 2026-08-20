b73beda376d7623739fb833552212ffc7f141cd0
GAJBA Studio <108943540+nerminvkl@users.noreply.github.com>
2026-08-20 22:32:03 +0200

    Add pos template

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-fallback-samo-za-dev")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 # SECURITY WARNING: don't run with debug turned on in production!
