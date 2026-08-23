6fd802f6e5efdfde4f8d385a90b993fafdc3c69b
GAJBA Studio <108943540+nerminvkl@users.noreply.github.com>
2026-08-23 11:52:30 +0200

    Remove decouple, use os.environ

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
 
-SECRET_KEY = config("SECRET_KEY")
-DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-fallback")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
