6d6fa2ce3de81b744b1a246fa08ad6db18033e28
GAJBA Studio <108943540+nerminvkl@users.noreply.github.com>
2026-08-23 11:55:12 +0200

    Add pd.mnmlogic.com to default ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-fallback")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,pd.mnmlogic.com").split(",")
 
 
 # Application definition
