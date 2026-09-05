5514cbef7e405dbd8df78780eb5c77c26bf977bc
GAJBA Studio <108943540+nerminvkl@users.noreply.github.com>
2026-09-05 11:59:30 +0200

    Custom serve_media view

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,15 +1,21 @@
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.urls import path, include, re_path
-from django.views.static import serve
+from django.http import FileResponse, Http404
+import os
+
+def serve_media(request, path):
+    full_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(full_path) and os.path.isfile(full_path):
+        return FileResponse(open(full_path, 'rb'))
+    raise Http404
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls")),
     path("", include("shop.urls")),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve_media),
 ]
 
 if settings.DEBUG:
