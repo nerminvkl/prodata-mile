60cf4ced866c9dc1a5495060239d14c87d825aa3
GAJBA Studio <108943540+nerminvkl@users.noreply.github.com>
2026-09-05 11:47:55 +0200

    Serve media in production

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,18 +1,19 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls")),
     path("", include("shop.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
